7bfabe54cde178b573d8679f31934bb696478b9a
AlexW2311 <alex.williams2311@icloud.com>
2024-03-06 16:27:25 -0800

    Inital commit

---
+++ ch3renderPipeline/ch3renderPipeline/ch3renderPipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ch3renderPipelineApp.swift
+//  ch3renderPipeline
+//
+//  Created by Alexander Williams on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ch3renderPipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
